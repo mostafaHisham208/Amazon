f8974a7d8c1e383d7d11c1306972b37bf8d8ab22
Mohammad Zaki <mz.26925787@gmail.com>
2023-06-08 13:03:49 -0700

    add navbar,menu side , sign page and register page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Sign from "./Pages/Sign/Sign";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <>
-
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign" element={<Sign/>} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
