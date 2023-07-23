bf6d85909bcdaddfaff3cf53c21e0009329cd4d4
Mostafa Hisham <hmostafaharpy22@gmail.com>
2023-07-23 19:53:51 +0300

    edit style

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,48 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
-import logo from "../../assets/images/amazon-logo.svg";
-import { Link, Navigate } from "react-router-dom";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../FirebaseConfig/FirebaseConfig";
+import logo from "../../assets/images/amazon-icon-21121.jpg";
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  function handleRegister(e) {
-    // e.preventDefault();
-   
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user.email);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-    // Navigate("/");
-  }
   return (
     <>
       <div>
         <div className="register">
-          <img src={logo} id="amazon-icon" alt="" />
+          <img src={logo} id="amazon-icon" />
           <div className="card">
-            <form id="form" onSubmit={(e) => handleRegister(e)}>
+            <form action="/ThankPage/thank.html" method="get" id="form">
               <h1>Create account</h1>
               <label htmlFor="name">Your Name</label>
               <input type="text" id="name" placeholder="First and Last name" />
               <label htmlFor="mobile">Mobile number or email</label>
-              <input type="text" id="mobile" onChange={(e) => setEmail(e.target.value)}  value={email}/>
+              <input type="text" id="mobile" />
               <label htmlFor="password">Password</label>
               <input
                 type="password"
                 id="password"
                 placeholder="At least 6 Characters"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
               />
               <div className="alert">
                 <i className="fa-solid fa-info" style={{ color: "#629dba" }} />
@@ -61,10 +37,10 @@ export default function Register() {
             </p>
             <div className="helping">
               <div className="signIn">
-                Already have an account?<Link to="/Sign">Sign in</Link>
+                Already have an account?<a href="/Sign.html">Sign in</a>
                 <i
                   className="fa-solid fa-caret-right"
-                  style={{ "fontSize": "10px", color: "#0066c0" }}
+                  style={{ "font-size": "10px", color: "#0066c0" }}
                 />
               </div>
             </div>
